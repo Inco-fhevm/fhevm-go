6543c8dda91f9131a87004237acf673692028a85
LoveNoble <elmer@inco.network>
2024-04-23 20:08:43 +0200

    feat: add div, rem operations

--- fhevm/tee_arthmetic_gas.go
+++ fhevm/tee_arthmetic_gas.go
@@ -39,3 +39,41 @@ func teeMulRequiredGas(environment EVMEnvironment, input []byte) uint64 {
 
 	return environment.FhevmParams().GasCosts.TeeMul[lhs.fheUintType()]
 }
+
+func teeDivRequiredGas(environment EVMEnvironment, input []byte) uint64 {
+	input = input[:minInt(65, len(input))]
+
+	logger := environment.GetLogger()
+
+	var lhs, rhs *verifiedCiphertext
+	lhs, rhs, err := get2VerifiedOperands(environment, input)
+	if err != nil {
+		logger.Error("teeDiv RequiredGas() ciphertext inputs not verified", "err", err, "input", hex.EncodeToString(input))
+		return 0
+	}
+	if lhs.fheUintType() != rhs.fheUintType() {
+		logger.Error("teeDiv RequiredGas() operand type mismatch", "lhs", lhs.fheUintType(), "rhs", rhs.fheUintType())
+		return 0
+	}
+
+	return environment.FhevmParams().GasCosts.TeeDiv[lhs.fheUintType()]
+}
+
+func teeRemRequiredGas(environment EVMEnvironment, input []byte) uint64 {
+	input = input[:minInt(65, len(input))]
+
+	logger := environment.GetLogger()
+
+	var lhs, rhs *verifiedCiphertext
+	lhs, rhs, err := get2VerifiedOperands(environment, input)
+	if err != nil {
+		logger.Error("teeRem RequiredGas() ciphertext inputs not verified", "err", err, "input", hex.EncodeToString(input))
+		return 0
+	}
+	if lhs.fheUintType() != rhs.fheUintType() {
+		logger.Error("teeRem RequiredGas() operand type mismatch", "lhs", lhs.fheUintType(), "rhs", rhs.fheUintType())
+		return 0
+	}
+
+	return environment.FhevmParams().GasCosts.TeeRem[lhs.fheUintType()]
+}
